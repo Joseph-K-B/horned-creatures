82b3d2d791abe39dffd492c2f73b8cb6a63b6569
Joseph-K-B <303josephb@gmail.com>
2021-08-04 16:43:53 -0700

    cleaning up code

--- src/App.js
+++ src/App.js
@@ -50,21 +50,3 @@ class App extends Component{
     }
     
     export default App;
-    
-    /* <select onChange={this.keywordChange}>
-      <option value='All'>All</option>
-      <option value='narwhal'>narwhal</option>
-      <option value='rhino'>rhino</option>
-      <option value='unicorn'>unicorn</option>
-      <option value='unilego'>unilego</option>
-      <option value='triceratops'>triceratops</option>
-      <option value='markhor'>markhor</option>
-      <option value='mouflon'>mouflon</option>
-      <option value='addax'>addax</option>
-      <option value='chameleon'>chameleon</option>
-      <option value='lizard'>lizard</option>
-      <option value='dragon'></option>
-      
-    </select>
-
-    < ImageList horns={filterKeys} /> */
